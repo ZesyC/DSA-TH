eb62495372729736599654137494e62b81f7c012
ZesyC <24100012@st.phenikaa-uni.edu.vn>
2025-10-22 16:25:45 +0700

    sang commit

--- lt/stackmang.c
+++ lt/stackmang.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #define max_size 100
 
-typedef struct Stack{
+typedef struct Stack
+{
     int arr[max_size];
     int top;
 } Stack;
 
-void push(int val, int arr[], int top) {
-    if(top < max) {
-        arr[top] = val;
+void initStack(Stack *s)
+{
+    s->top = 0;
+}
+
+bool isEmpty(Stack *s)
+{
+    return s->top == 0;
+}
+
+bool isFull(Stack *s)
+{
+    return s->top == max_size;
+}
+
+void push(int val, int arr[], int top)
+{
+    if (top < max_size)
+    {
         top++;
-    } else {
+        arr[top] = val;
+    }
+    else
+    {
         printf("Stack Overflow\n");
     }
+}
+
+int pop_back(int arr[max_size], int top)
+{
+    return arr[top];
 }
